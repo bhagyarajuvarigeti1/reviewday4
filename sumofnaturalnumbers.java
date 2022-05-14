fe770890ff5401e84f3401b4afbb1de5e038bef4
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-05-14 12:35:34 +0530

    [bhagya] sum of n natural numbers

---
+++ sumofnaturalnumbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class main{
+	public static void main(String[] args){
+	 Scanner in=new Scanner(System.in);
+	int x=in.nextInt();
+	for(int i=0;i<n+1;i++){
+	sum+=i;
+	}
+	System.out.println("Sum of natural number of " +x +"is   "+sum);
+}}
+
